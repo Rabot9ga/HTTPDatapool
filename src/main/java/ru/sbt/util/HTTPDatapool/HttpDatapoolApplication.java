4d3ebced3d8d25416dc37a6086a706d29f8c7e32
Кленин Даниил <sbt-klenin-da@mail.ca.sbrf.ru>
2018-01-10 14:33:27 +0300

    init

---
+++ src/main/java/ru/sbt/util/HTTPDatapool/HttpDatapoolApplication.java
@@ -0,0 +1,12 @@
+package ru.sbt.util.HTTPDatapool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpDatapoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpDatapoolApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/sbt/util/HTTPDatapool/HttpDatapoolApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.sbt.util.HTTPDatapool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttpDatapoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
