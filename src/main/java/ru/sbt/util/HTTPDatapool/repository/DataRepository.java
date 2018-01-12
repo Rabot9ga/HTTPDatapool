24cd91ff0c0e4d7c64871ca1c70be4e9cdb1691a
Мареев Иван Олегович <SBT-Mareev-IO@mail.ca.sbrf.ru>
2018-01-12 10:52:23 +0300

    Cast to String, метод для вытягивания из кэша

--- src/main/java/ru/sbt/util/HTTPDatapool/repository/DataRepository.java
+++ src/main/java/ru/sbt/util/HTTPDatapool/repository/DataRepository.java
@@ -1,5 +1,6 @@
 package ru.sbt.util.HTTPDatapool.repository;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,6 +10,7 @@ import ru.sbt.util.HTTPDatapool.connectionInterface.DBConnection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Repository
 public class DataRepository implements DBConnection {
@@ -17,21 +19,34 @@ public class DataRepository implements DBConnection {
     private JdbcTemplate jdbcTemplate;
 
     @Override
-    public List<Map<String, Object>> getTable(String tableName) throws DataAccessException {
-        List<Map<String, Object>> result = jdbcTemplate.queryForList("SELECT * FROM "+tableName);
-        return result;
+    public List<Map<String, String>> getTable(String tableName) throws DataAccessException {
+        return jdbcTemplate.queryForList("SELECT * FROM "+tableName).stream()
+                .map(this::castMapValue)
+                .collect(Collectors.toList());
 
     }
 
     @Override
-    public List<Map<String, Object>> getSomeColumnFromTable(String tablename, Set<String> columnNames) throws DataAccessException {
+    public List<Map<String, String>> getSomeColumnFromTable(String tablename, Set<String> columnNames) throws DataAccessException {
         String query = "SELECT ";
         for (String columnName : columnNames) {
             query+=columnName +", ";
         }
         query=query.substring(0,query.lastIndexOf(','));
         query+=" FROM "+tablename;
-        List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
-        return result;
+        return jdbcTemplate.queryForList(query).stream()
+                .map(this::castMapValue)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Map<String, String>> getDataFromCache(String tablenamem, Set<String> columnNames) {
+        return null;
+    }
+
+    private Map<String, String> castMapValue(Map<String, Object> map) {
+        return map.entrySet().stream()
+                .map(entry -> ImmutablePair.of(entry.getKey(), entry.getValue().toString()))
+                .collect(Collectors.toMap(o -> o.left, o -> o.right));
     }
 }
